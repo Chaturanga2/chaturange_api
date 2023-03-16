e07af934feba9b1ec1c63f585de445ba736a4d8e
laurabjn <92058813+laurabjn@users.noreply.github.com>
2023-03-16 12:58:38 +0100

    feat: 🎸 redis cache manager

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule, CacheStore } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import * as process from 'process';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
+    CacheModule.register({ 
+      store: redisStore as unknown as CacheStore,
+      host: 'localhost',
+      port: 6379,
+      isGlobal: true
+    }),
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
     }),
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    //MongooseModule.forRoot(process.env.MONGO_URI)
   ],
   controllers: [AppController],
   providers: [AppService],
