d3fd4812c8cfbd8e5810ffbf263467365b006f33
laurabjn <92058813+laurabjn@users.noreply.github.com>
2023-03-17 09:16:42 +0100

    refactor: 💡 change meaning of the code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module, CacheModule, CacheStore } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,15 +8,15 @@ import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
     CacheModule.register({ 
       store: redisStore,
       host: process.env.REDIS_HOST,
       port: process.env.REDIS_PORT,
       isGlobal: true
-    }),
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.MONGO_URI)
   ],
