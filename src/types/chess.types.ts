86b96e79286d9cc468cd1cad9f65cd7413f093ee
Matleftod <97225801+Matleftod@users.noreply.github.com>
2023-04-06 11:32:07 +0200

    Update chessType

---
+++ src/types/chess.types.ts
@@ -0,0 +1,15 @@
+export type Piece = {
+    image?: string;
+    symbol: string;
+    color: string;
+    moved: boolean;
+  } | null;
+  
+  export type CellType = {
+    x: number;
+    y: string;
+    piece: Piece;
+  };
+  
+  export type Board = CellType[][];
+  
