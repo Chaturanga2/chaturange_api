46da6d58c8138eec2b35982686411873eb3544d5
laurabjn <92058813+laurabjn@users.noreply.github.com>
2023-04-06 16:15:19 +0200

    feat: 🎸 queenMove and bishopMove improve

--- src/chess-game/chess-game.service.ts
+++ src/chess-game/chess-game.service.ts
@@ -8,6 +8,116 @@ export class ChessGameService {
 
   public readonly board: CellType[][] = [];
 
+  public moveBishop(
+    board: CellType[][],
+    oldCell: CellType,
+    newCell: CellType,
+    currentPlayer: string,
+  ): boolean {
+    const bishop = oldCell.piece;
+
+    // Vérifier si la case de départ contient un fou de la bonne couleur
+    if (!bishop || bishop.color !== currentPlayer || bishop.symbol !== 'b') {
+      return false;
+    }
+
+    // Vérifier si le déplacement est valide
+    const xDiff = Math.abs(oldCell.x - newCell.x);
+    const yDiff = Math.abs(
+      this.y_axis.indexOf(oldCell.y) - this.y_axis.indexOf(newCell.y),
+    );
+    if (xDiff !== yDiff) {
+      return false;
+    }
+    const xDirection = oldCell.x < newCell.x ? 1 : -1;
+    const yDirection =
+      this.y_axis.indexOf(oldCell.y) < this.y_axis.indexOf(newCell.y) ? 1 : -1;
+    for (let i = 1; i < x; i++) {
+      const currentCell =
+        board[oldCell.x + i * xDirection][
+          this.y_axis.indexOf(oldCell.y) + i * yDirection
+        ];
+      if (currentCell.piece) {
+        return false;
+      }
+    }
+
+    // Déplacement
+    newCell.piece = oldCell.piece;
+    oldCell.piece = null;
+
+    return true;
+  }
+
+  public moveQueen(
+    board: CellType[][],
+    oldCell: CellType,
+    newCell: CellType,
+    currentPlayer: string,
+  ): boolean {
+    const queen = oldCell.piece;
+
+    // Vérifier si la case de départ contient une reine de la bonne couleur
+    if (!queen || queen.color !== currentPlayer || queen.symbol !== 'q') {
+      return false;
+    }
+
+    // Vérifier si le déplacement est valide
+    if (oldCell.x === newCell.x) {
+      // Déplacement horizontal
+      const min = Math.min(
+        this.y_axis.indexOf(oldCell.y),
+        this.y_axis.indexOf(newCell.y),
+      );
+      const max = Math.max(
+        this.y_axis.indexOf(oldCell.y),
+        this.y_axis.indexOf(newCell.y),
+      );
+      for (let i = min + 1; i < max; i++) {
+        if (board[oldCell.x][i].piece) {
+          return false;
+        }
+      }
+    } else if (oldCell.y === newCell.y) {
+      // Déplacement vertical
+      const min = Math.min(oldCell.x, newCell.x);
+      const max = Math.max(oldCell.x, newCell.x);
+      for (let i = min + 1; i < max; i++) {
+        if (board[i][this.y_axis.indexOf(oldCell.y)].piece) {
+          return false;
+        }
+      }
+    } else {
+      // Déplacement diagonal
+      const xDiff = Math.abs(oldCell.x - newCell.x);
+      const yDiff =
+        this.y_axis.indexOf(oldCell.y) - this.y_axis.indexOf(newCell.y);
+      if (xDiff !== yDiff) {
+        return false;
+      }
+      const xDirection = oldCell.x < newCell.x ? 1 : -1;
+      const yDirection =
+        this.y_axis.indexOf(oldCell.y) < this.y_axis.indexOf(newCell.y)
+          ? 1
+          : -1;
+      for (let i = 1; i < xDiff; i++) {
+        if (
+          board[oldCell.x + i * xDirection][
+            this.y_axis.indexOf(oldCell.y) + i * yDirection
+          ].piece
+        ) {
+          return false;
+        }
+      }
+    }
+
+    // Déplacement
+    newCell.piece = queen;
+    oldCell.piece = null;
+
+    return true;
+  }
+
   public moveKing(
     board: CellType[][],
     oldCell: CellType,
@@ -25,15 +135,12 @@ export class ChessGameService {
     const xDiff = oldCell.x === newCell.x ? 0 : 1;
     const yDiff =
       this.y_axis.indexOf(oldCell.y) - this.y_axis.indexOf(newCell.y);
-    const y = this.y_axis[this.y_axis.indexOf(oldCell.y) - yDiff];
     if (xDiff > 1 || yDiff > 1) {
       return false;
     }
 
     // Déplacement
     newCell.piece = king;
-    newCell.x = oldCell.x + xDiff;
-    newCell.y = y;
     oldCell.piece = null;
 
     return true;
