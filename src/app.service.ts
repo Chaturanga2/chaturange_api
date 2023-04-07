3705341baa7453dc1b0eee8d0176e118c55f20fa
Julien Paillassa <julien-paillassa_student2021@wilder.school>
2023-04-07 13:57:19 +0200

    merge branch reset into main

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello My Friend!';
+    return 'Hello My Friend!!';
   }
 }
